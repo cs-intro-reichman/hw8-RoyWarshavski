91130230a1159c75784aecdf0079b424066f8b7f
Roy Warshavski <roywarshavski@Roys-MacBook-Pro.local>
2026-01-04 11:33:17 +0200

    final

--- Network.java
+++ Network.java
@@ -131,11 +131,15 @@ public class Network {
     }
 
     // Returns a textual description of all the users in this network, and who they follow.
-    public String toString() {
+   public String toString() {
     String ans = "Network:\n";
     for (int i = 0; i < userCount; i++) {
-        ans = ans + users[i].toString() + "\n";
+        ans += users[i].toString();
+        if (i < userCount - 1) {
+            ans += "\n";
+        }
     }
     return ans;
 }
+
 }
